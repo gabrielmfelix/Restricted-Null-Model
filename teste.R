23f6bb8e62e7fc8f743907f8b53ce3c60caf1a76
Marco Mello <marmello@usp.br>
2020-09-08 11:44:20 -0300

    Teste.R
    Mexi mais um pouco no arquivo de teste, mas não concluí o que queria fazer. Ainda quero testar com a net1.

--- teste.R
+++ teste.R
@@ -1,10 +1,71 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-rm(list= ls())
+
 cat("\014")  
 
+# Teste com net1
+
+net1 <- read.delim("net1.txt", row.names = 1)
+net1
+
+library(bipartite)
+
+net1 = read.delim("net1.txt", row.names=1)
+net1
+
+net1.lpa = computeModules(net1, method = "Beckett")
+net1.lpa
+printoutModuleInformation(net1.lpa)
+modulos.lpa.lista = listModuleInformation(net1.lpa)
+source("Extracting_modules_from_bipartite.R")
+modules.lpa.dataframe = modules_from_bipartite(modulos.lpa.lista)
+colnames(modules.lpa.dataframe$Rows_modules)=c("nodes", "modules")
+colnames(modules.lpa.dataframe$Cols_modules)=c("nodes", "modules")
+membership.lpa = rbind(modules.lpa.dataframe$Rows_modules, modules.lpa.dataframe$Cols_modules)
+membership.lpa
+write.table(membership.lpa, "partititions.txt", sep="\t", quote=F)
+
+rowspart = read.delim("partitions.rows.txt", header = T)
+colspart = read.delim("partitions.cols.txt", header = T)
+
+rowspart2 <- rowspart$rows
+colspart2 <- colspart$cols
+
+dim(net1)
+length(rowspart2)
+length(colspart2)
+
+
+net1.nes <- sortmatrix(net1, topology = "nested")
+net1.nes
+
+net1.com <- sortmatrix(net1, topology = "compound",
+                          row_partitions = rowspart2,
+                          col_partitions = colspart2)
+net1.com
+
+visweb(net1)
+
+modcol <- c("purple", "orange", "yellow", "blue")
+
+plotmatrix(net1.nes, binary = F, base_color = "blue")
+
+plotmatrix(net1.com, binary = F, border = T, 
+           modules_colors = modcol,
+           within_color = modcol)
+
+
+mod <- module2constraints(net1.lpa)
+
+nest.smdm(net1, constraints = mod, 
+          weighted = T, 
+          decreasing = "abund", 
+          sort = T)
+
+
+
+# Teste com matriz gerada automaticamente 
 
 set.seed(3)
-
 
 matriz <- matrix(
   (sample(0:1000, 144, replace = T)),
