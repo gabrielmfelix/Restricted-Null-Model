733522c8b5b186d51489bac23d7745c807fced4a
Marco Mello <marmello@usp.br>
2020-09-08 13:42:21 -0300

    Novidades no teste.R
    Desenvolvi melhor o exemplo, integrando os diferentes códigos comentados na issue #2 . Falta agora a parte final, ou seja, calcular os aninhamentos para as matrizes randomizadas, fazer a contagem de quantos estão acima e abaixo dos aninhamentos da matriz original, e fechar calculando com os valores de P.

--- teste.R
+++ teste.R
@@ -1,69 +1,79 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-cat("\014")  
-
-# Teste com net1
-
-net1 <- read.delim("net1.txt", row.names = 1)
-net1
+cat("\014") 
 
 library(bipartite)
+source("RestNullModel.R")
+source("PosteriorProb.R")
+source("modules_from_bipartite.R")
 
-net1 = read.delim("net1.txt", row.names=1)
+
+##### Teste com net1 #####
+
+net1 <- as.matrix(read.delim("net1.txt", row.names = 1))
 net1
+class(net1)
+visweb(net1)
 
-net1.lpa = computeModules(net1, method = "Beckett")
+net1.lpa = computeModules(net1b, method = "Beckett")
 net1.lpa
 printoutModuleInformation(net1.lpa)
 modulos.lpa.lista = listModuleInformation(net1.lpa)
-source("Extracting_modules_from_bipartite.R")
 modules.lpa.dataframe = modules_from_bipartite(modulos.lpa.lista)
 colnames(modules.lpa.dataframe$Rows_modules)=c("nodes", "modules")
 colnames(modules.lpa.dataframe$Cols_modules)=c("nodes", "modules")
 membership.lpa = rbind(modules.lpa.dataframe$Rows_modules, modules.lpa.dataframe$Cols_modules)
 membership.lpa
-write.table(membership.lpa, "partititions.txt", sep="\t", quote=F)
 
-rowspart = read.delim("partitions.rows.txt", header = T)
-colspart = read.delim("partitions.cols.txt", header = T)
+rowspart = modules.lpa.dataframe$Rows_modules
+colspart = modules.lpa.dataframe$Cols_modules
 
-rowspart2 <- rowspart$rows
-colspart2 <- colspart$cols
+rowspart2 <- rowspart$modules
+class(rowspart2)
+colspart2 <- colspart$modules
+class(colspart2)
 
 dim(net1)
 length(rowspart2)
 length(colspart2)
-
-
-net1.nes <- sortmatrix(net1, topology = "nested")
-net1.nes
 
 net1.com <- sortmatrix(net1, topology = "compound",
                           row_partitions = rowspart2,
                           col_partitions = colspart2)
 net1.com
 
-visweb(net1)
-
-modcol <- c("purple", "orange", "yellow", "blue")
-
-plotmatrix(net1.nes, binary = F, base_color = "blue")
-
+modcol <- rainbow((length(unique(membership.lpa$modules))), alpha=1)
 plotmatrix(net1.com, binary = F, border = T, 
            modules_colors = modcol,
            within_color = modcol)
 
-
 mod <- module2constraints(net1.lpa)
-
 nest.smdm(net1, constraints = mod, 
           weighted = T, 
           decreasing = "abund", 
           sort = T)
 
+probabilities <- PosteriorProb(net1,
+                              rowspart2,
+                              colspart2,
+                              Prior.Pij = "degreeprob.byarea",
+                              Conditional.level = "modules")
+probabilities
 
 
-# Teste com matriz gerada automaticamente 
+randomized <- RestNullModel(net1, probabilities, Numbernulls = 1000, 
+                         Print.null = T, allow.degeneration = T,
+                         return.nonrm.species = T, connectance = T,
+                         byarea = F, R.partitions = F, C.partitions = F)
+
+randomized
+
+
+
+
+
+
+##### Teste com matriz gerada automaticamente #####
 
 set.seed(3)
 
